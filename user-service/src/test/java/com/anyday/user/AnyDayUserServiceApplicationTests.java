b8cbd51a342b8967b873a2156950f1bce8c3607e
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2024-06-28 01:03:19 +0900

    (ANYDAY-2) feat: add initial user service application

---
+++ user-service/src/test/java/com/anyday/user/AnyDayUserServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.anyday.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyDayUserServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
