48edf8e5cc62108fdd7681c8d355959e831f68bc
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2024-06-30 00:24:44 +0900

    (ANYDAY-4) feat: add initial auth service application (#4)
    * (ANYDAY-4) feat: add initial auth service application
    
    * (ANYDAY-4) ci: fix build script error
    
    * (ANYDAY-4) style: fix formatting error

---
+++ auth-service/src/test/java/com/anyday/auth/AnyDayAuthServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.anyday.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyDayAuthServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
